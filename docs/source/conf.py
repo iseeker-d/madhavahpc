8ed160a9124e29eaab172c0c01cb93041f83d83d
iseeker-d <93239514+iseeker-d@users.noreply.github.com>
2022-03-10 16:41:36 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Madhava HPC'
+copyright = '2022, NITC'
+author = 'sajil'
 
 release = '0.1'
 version = '0.1.0'
